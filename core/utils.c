0c2bac7b33a8507a92979f1dcf2d65dbe5e5863a
Doyle Huang <doyle.sy.huang@mail.foxconn.com>
2017-12-20 15:59:17 +0800

    add sleep
    Signed-off-by: Doyle Huang <doyle.sy.huang@mail.foxconn.com>

--- core/utils.c
+++ core/utils.c
@@ -1,4 +1,7 @@
 #include <uwsgi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 
 extern struct uwsgi_server uwsgi;
@@ -1151,7 +1154,7 @@ void uwsgi_close_request(struct wsgi_request *wsgi_req) {
 
 	// defunct process reaper
 	if (uwsgi.reaper == 1) {
-		while (waitpid(WAIT_ANY, &waitpid_status, WNOHANG) > 0);
+		while (waitpid(WAIT_ANY, &waitpid_status, WNOHANG) > 0) {usleep(100000);};
 	}
 	uwsgi_log("==> Doyle== %s, %d\n", __FUNCTION__, __LINE__);
 
